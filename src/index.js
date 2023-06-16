c6352d899be667f0a4481c6fa0c105703080298c
Grant Collins <grantcollins03@gmail.com>
2023-06-15 20:36:51 -0400

    create mainView.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import './styles.css';
 import { allTasks, Task, allProjects, Project, inbox } from './modules/app';
 import createSidebar from './modules/sidebar';
-
-
-createSidebar().toggle();
-
-console.log(allTasks);
-console.log(inbox);
+import { createViewContainer } from './modules/mainView';
 
 
 const newProject = new Project ('Gym', 'Things to fill 8 hours with');
 
 console.log(newProject.getTasks());
+
+createSidebar();
+createViewContainer(newProject);
+
+
+
+console.log(allTasks);
 console.log(inbox);
-// newTask2.delete();
-// console.log(allTasks);
 
-// const newProject = new Project('Gym', 'Time to get fit');
-// newProject.add();
-// const newProject2 = new Project('Work', 'Things for the slog');
-// newProject2.add();
-// console.log(newProject);
-// console.log(allProjects);
+console.log(newProject.getTasks());
+console.log(inbox);
 
-// newProject.delete();
-// console.log(allProjects);
 
 
 
--- src/modules/mainView.js
+++ src/modules/mainView.js
@@ -5,7 +5,75 @@ const app = d.getElementById('app');
 const mainContainer = app.appendChild(d.createElement('section'));
 mainContainer.classList.add('main-container');
 
+function createViewContainer(view) {
+    // view is an object, such as a project, that contains the title, description, and tasks
+    const viewContainer = mainContainer.appendChild(d.createElement('div'));
+    viewContainer.classList.add('view-container');
+
+    const viewName = viewContainer.appendChild(d.createElement('h2'));
+    viewName.id = 'view-name'; //maybe don't need this...
+    viewName.textContent = view.title;
+
+    const viewDescription = viewContainer.appendChild(d.createElement('p'));
+    viewDescription.id = 'view-description'; //maybe don't need this...
+    viewDescription.textContent = view.description;
+
+    const createTasksContainer = (function() {
+        const tasksContainer = viewContainer.appendChild(d.createElement('div'));
+        tasksContainer.classList.add('tasks-container');
+
+        const createTasks = (function() {
+            for (let i = 0; i < view.getTasks().length; i++) {
+                let taskObj = view.getTasks()[i];
+
+                const taskCard = tasksContainer.appendChild(d.createElement('div'));
+                taskCard.classList.add('task');
+
+                const completeBtn = taskCard.appendChild(d.createElement('button'));
+                completeBtn.classList.add('complete-btn');
+                completeBtn.innerHTML = `<i class="fa-regular fa-circle"></i>`;
+
+                const createPrimaryInfo = (function() {
+                    const primaryInfo = taskCard.appendChild(d.createElement('div'));
+                    primaryInfo.classList.add('primary-info');
+
+                    const name = primaryInfo.appendChild(d.createElement('div'));
+                    name.classList.add('name');
+                    name.textContent = taskObj.title;
+
+                    const description = primaryInfo.appendChild(d.createElement('div'));
+                    description.classList.add('description');
+                    description.textContent = taskObj.description;
+
+                })();
+
+                const createMetaContainer = (function() {
+                    const metaContainer = taskCard.appendChild(d.createElement('ul'));
+                    metaContainer.classList.add('meta-container');
+    
+                    const priorityTag = metaContainer.appendChild(d.createElement('li'));
+                    priorityTag.classList.add('meta-tag');
+                    // priorityTag.classList.add('') how to add priorty class? with switch?
+                    priorityTag.textContent = taskObj.priority;
+    
+                    const projectTag = metaContainer.appendChild(d.createElement('li'));
+                    projectTag.classList.add('meta-tag');
+                    projectTag.textContent = taskObj.project;
+                    
+                    const dueDateTag = metaContainer.appendChild(d.createElement('li'));
+                    dueDateTag.classList.add('meta-tag');
+                    dueDateTag.textContent = taskObj.dueDate;
+                })();
+            } 
+        })();
+    })();
+
+    const addTaskBtn = viewContainer.appendChild(d.createElement('button'));
+    addTaskBtn.id = 'add-task-btn';
+    addTaskBtn.innerHTML = `<i class="fa-solid fa-plus"></i>Add Task`;
+
+    return viewContainer;
+}
 
 
-
-export { d, mainContainer }
+export { d, mainContainer, createViewContainer };
