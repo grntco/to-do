a966ebace04e3228fa1f9eeefba39ae9272dfbd7
Grant Collins <grantcollins03@gmail.com>
2023-06-20 16:34:38 -0400

    add viewContainer.js/rename mainContainer.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import { allTasks, Task, allProjects, Project, inboxView, todayView, weekView } from './modules/app';
 import createNav from './modules/nav';
 import createSidebar from './modules/sidebar';
-import { createViewContainer } from './modules/mainContainer';
+import createViewContainer from './modules/viewContainer';
 import events from './modules/events';
 
 const newProject = new Project ('Gym', 'Things to fill 8 hours with');
