50be0f5e84eaf20041c4407671b993bd05a0656f
Grant Collins <grantcollins03@gmail.com>
2023-08-18 13:17:22 -0400

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import './styles.css';
-import { allTasks, Task, allProjects, Project, inboxView, todayView, weekView } from './modules/app';
+import { inboxView } from './modules/app';
 import createNav from './modules/nav';
 import { createSidebar } from './modules/sidebar';
-import { createViewContainer, removeViewContainer } from './modules/viewContainer';
+import { createViewContainer } from './modules/viewContainer';
 import events from './modules/events';
-import { saveToLocalStorage, getFromLocalStorage } from './modules/localStorage';
 
-saveToLocalStorage(allProjects);
-console.log(allTasks);
-
-getFromLocalStorage(allTasks);
-
-
-
-createNav();
-createSidebar();
-createViewContainer(inboxView);
+const init = (function() {
+    createNav();
+    createSidebar();
+    createViewContainer(inboxView);
+})();
