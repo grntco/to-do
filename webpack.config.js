d9c52d062ef02dfe7902cde079f372a6188e9fbe
Grant Collins <grantcollins03@gmail.com>
2023-09-05 09:03:56 -0400

    add babel.js support

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
     ],
   },
 };
